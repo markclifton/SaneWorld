d6eb4a720513213fdbd2f6eb1dd883904d3832e6
Mark Clifton <m.clifton94@live.com>
2021-09-28 12:40:54 -0700

    GameWindow Rendering fix

--- src/Sandbox.cpp
+++ src/Sandbox.cpp
@@ -1,27 +1,13 @@
-
-#include <shader_code.hpp>
-
 #include <sane/entrypoint.hpp>
-#include <sane/core/display.hpp>
-#include <sane/graphics/buffer.hpp>
-#include <sane/graphics/shaderprogram.hpp>
-#include <sane/graphics/framebuffer.hpp>
-#include <sane/graphics/texture.hpp>
-#include <sane/logging/log.hpp>
 #include <sane/layers/console.hpp>
 #include <sane/layers/fpscounter.hpp>
 #include <sane/layers/gamewindow.hpp>
-
-#include <sane/rendering/renderer2d.hpp>
-
-#include <sane/math/vector.hpp>
 
 class Sandbox : public Sane::App
 {
 public:
   Sandbox()
     : Sane::App("Sandbox")
-    , console()
     , gameWindow(framebuffer.GetAttachment(0))
   {
     PushOverlay(&console);
