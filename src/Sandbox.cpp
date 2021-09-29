4f951e3c11412342529360191fe1c816780d3682
Mark Clifton <m.clifton94@live.com>
2021-09-28 22:21:00 -0400

    Update to movement system

--- src/Sandbox.cpp
+++ src/Sandbox.cpp
@@ -3,8 +3,10 @@
 #include <sane/ecs/components/scale.hpp>
 #include <sane/ecs/components/player.hpp>
 #include <sane/ecs/components/translation.hpp>
-#include <sane/ecs/systems/renderer2d.hpp>
+#include <sane/ecs/systems/collision.hpp>
+#include <sane/ecs/systems/physics.hpp>
 #include <sane/ecs/systems/movement.hpp>
+#include <sane/ecs/systems/renderer2d.hpp>
 #include <sane/layers/layer.hpp>
 #include <sane/layers/console.hpp>
 #include <sane/layers/fpscounter.hpp>
@@ -18,6 +20,8 @@ public:
     , gameWindow(framebuffer.GetAttachment(0))
     , renderer(Registry())
     , movementSystem(Registry())
+    , physicsSystem(Registry())
+    , collisionSystem(Registry())
   {
     PushOverlay(&console);
     PushOverlay(&gameWindow);
@@ -25,19 +29,24 @@ public:
 
     const auto entity = Registry().create();
     Registry().emplace<Sane::Components::Player>(entity);
-    Registry().emplace<Sane::Components::Position>(entity, 1.f, 0.f, 0.f, 1.f);
+    Registry().emplace<Sane::Components::Position>(entity, 0.f, 0.f, 0.f, 1.f);
     Registry().emplace<Sane::Components::Translation>(entity, 0.f, 0.f, 0.f);
-    Registry().emplace<Sane::Components::Scale>(entity, .05f, .05f, 1.f);
+    Registry().emplace<Sane::Components::Scale>(entity, .1f, .1f, 1.f);
+    Registry().emplace<Sane::Components::Collidable>(entity, true);
+    Registry().emplace<Sane::Components::Gravity>(entity, .01f);
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 25; i++)
     {
       const auto entity = Registry().create();
-      Registry().emplace<Sane::Components::Position>(entity, -1.f + .25f * i, -1.f + .25f * i, 0.f, 1.f);
+      Registry().emplace<Sane::Components::Position>(entity, -1.f + .1f * i, -.25f, 0.f, 1.f);
       Registry().emplace<Sane::Components::Translation>(entity, 0.f, 0.f, 0.f);
       Registry().emplace<Sane::Components::Scale>(entity, .1f, .1f, .1f);
+      Registry().emplace<Sane::Components::Collidable>(entity, true);
     }
 
     PushSystem(&movementSystem);
+    PushSystem(&physicsSystem);
+    PushSystem(&collisionSystem);
     PushSystem(&renderer);
   }
 
@@ -51,6 +60,8 @@ private:
   Sane::GameWindow gameWindow;
   Sane::Systems::Renderer2d renderer;
   Sane::Systems::Movement movementSystem;
+  Sane::Systems::Physics physicsSystem;
+  Sane::Systems::Collision collisionSystem;
 };
 
 Sane::App* Sane::CreateApp()
